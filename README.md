25fcf4f290baf784029c9820a2cb06d147d13581
Gabriel Fabri <gcfabri@gmail.com>
2017-05-15 12:13:22 -0300

    Initial release

---
+++ README.md
@@ -0,0 +1,20 @@
+pipefy-node
+=========
+
+An unofficial Pipefy API wrapper using node.js
+
+## Installation
+
+    `npm install @gcfabri/pipefy-node`
+
+## Usage
+
+    `var pipefy = require('pipefy-node');`
+    or ES6/7
+    `import pipefy from 'pipefy-node';`
+
+## Tests
+
+    `npm test`
+
+
---
+++ test/test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+/*var sampleFunction = require('../index');
+
+describe('#sampleFunction', function() {
+    it('should do something', function() {
+        var result = sampleFunction();
+        expect(result).to.equal('1');
+    });
+});*/
